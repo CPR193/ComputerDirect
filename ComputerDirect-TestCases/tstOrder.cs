5759227be841a00f96bfece40059a42936f38078
Cosmin Rusu <P2441811@my365.dmu.ac.uk>
2020-02-07 15:47:06 +0000

    Edited Order and OrderLine tables Created test cases for Order and OrderLine Classes Created Order and OrderLine Classes

---
+++ ComputerDirect-TestCases/tstOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ComputerDirect_Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ComputerDirect_TestCases
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOK() 
+        {
+            clsOrder AnOrder = new clsOrder();
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+}
