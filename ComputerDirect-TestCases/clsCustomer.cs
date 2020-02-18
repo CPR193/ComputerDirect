0806c0e27db3215aed61776da9afec0d9da76bb6
Adnan Javid <P17219988@my365.dmu.ac.uk>
2020-02-18 12:57:07 +0000

    tstCustomer added

---
+++ ComputerDirect-TestCases/clsCustomer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ComputerDirect_TestCases
+{
+    internal class clsCustomer
+    {
+        public clsCustomer()
+        {
+        }
+
+        public string Address { get; internal set; }
+        public int CustomerID { get; internal set; }
+        public string CustomerName { get; internal set; }
+        public DateTime DateAdded { get; internal set; }
+    }
+}
---
+++ ComputerDirect-TestCases/tst.customer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ComputerDirect_TestCases
+{
+    [TestClass]
+    public class tst
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+
+            Assert.IsNotNull(ACustomer);
+        }
+
+        [TestMethod]
+        public void AddressOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+
+            String TestData = "";
+
+            ACustomer.Address = TestData;
+
+            Assert.AreEqual(ACustomer.Address, TestData);
+        }
+
+        [TestMethod]
+
+        public void CustomerIdOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+
+
+            Int32 TestData = 1;
+
+            ACustomer.CustomerID = TestData;
+
+            Assert.AreEqual(ACustomer.CustomerID, TestData);
+        }
+
+
+        [TestMethod]
+        public void CustomerNameOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+
+            String TestData = "";
+
+            ACustomer.CustomerName = TestData;
+
+            Assert.AreEqual(ACustomer.CustomerName, TestData);
+
+
+
+        [TestMethod]
+        public void DateCreatedOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+
+            DateTime TestData = DateTime.Now.Date;
+
+            ACustomer.DateAdded = TestData;
+
+            Assert.AreEqual(ACustomer.DateAdded, TestData);
+        }
+    }
+
+
+}
+    }
+
+}
