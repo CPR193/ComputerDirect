35b387d762620a0da63c6814a744d6d1d6f10566
Mahad Kurd <P2426628@my365.dmu.ac.uk>
2020-03-10 12:43:08 +0000

    update tb

---
+++ ComputerDirect-Classes/clsStaff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ComputerDirect_Classes
+{
+    public class clsStaff
+    {
+        public static string staffname;
+        public static string eMail;
+
+        public static int staffId { get; set; }
+        public static string Address { get; set; }
+        public static string PhoneNo { get; set; }
+        public bool Active { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
--- ComputerDirect-TestCases/tstStaff.cs
+++ ComputerDirect-TestCases/tstStaff.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ComputerDirect_Classes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ComputerDirect_TestCases
@@ -7,13 +8,101 @@ namespace ComputerDirect_TestCases
     public class tstStaff
     {
 
+
         [TestMethod]
         public void InstanceOK()
         {
             //create an instance of the class we want to create
-            clsAddress AnAddress = new clsAddress();
+            clsStaff AStaff = new clsStaff();
             //test to see that it exists
-            Assert.IsNotNull(AnAddress);
+            Assert.IsNotNull(AStaff);
+        }
+
+        [TestMethod]
+        public void ActivePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            AStaff.Active = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.Active, TestData);
+        }
+
+        [TestMethod]
+        public void DateOfBirthPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AStaff.DateOfBirth = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.DateOfBirth, TestData);
+
+        }
+        [TestMethod]
+        public void AddressPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            String TestData = "";
+            //assign the data to the property
+            clsStaff.Address = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(clsStaff.Address, TestData);
+        }
+        [TestMethod]
+        public void staffIdPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            clsStaff.staffId = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(clsStaff.staffId, TestData);
+        }
+        [TestMethod]
+        public void staffnamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            String TestData = "";
+            //assign the data to the property
+            clsStaff.staffname = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(clsStaff.staffname, TestData);
+        }
+        [TestMethod]
+        public void PhoneNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            String TestData = "";
+            //assign the data to the property
+            clsStaff.PhoneNo = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(clsStaff.PhoneNo, TestData);
+        }
+        [TestMethod]
+        public void eMailPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            String TestData = "";
+            //assign the data to the property
+            clsStaff.eMail = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(clsStaff.eMail, TestData);
         }
 
     }
